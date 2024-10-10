6632b0def1f363583fb2594f1ae3e834fa94763f
Muskan044 <muskankhan430430@gmail.com>
2024-10-10 19:11:54 +0500

    update formula in JS

--- script.js
+++ script.js
@@ -16,19 +16,20 @@ function calculateBmi() {
         return;
     }
 
-    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+    const bmi = weight / (height * height).toFixed(2);
+    const bmiCalculate = bmi.toFixed(2);
 
-    if (bmi < 18.5) {
-        result.innerText = `Under Weight: ${bmi}`;
-    } else if (bmi >= 18.5 && bmi < 24.9) {
-        result.innerText = `Normal: ${bmi}`;
-    } else if (bmi >= 25 && bmi < 29.9) {
-        result.innerText = `Over Weight: ${bmi}`;
-    } else if (bmi >= 30 && bmi < 34.9) {
-        result.innerText = `Obesity (Class I): ${bmi}`;
-    } else if (bmi >= 35.5 && bmi < 39.9) {
-        result.innerText = `Obesity (Class II): ${bmi}`;
+    if (bmiCalculate < 18.5) {
+        result.innerText = `Under Weight: ${bmiCalculate}`;
+    } else if (bmiCalculate >= 18.5 && bmiCalculate < 24.9) {
+        result.innerText = `Normal: ${bmiCalculate}`;
+    } else if (bmiCalculate >= 25 && bmiCalculate < 29.9) {
+        result.innerText = `Over Weight: ${bmiCalculate}`;
+    } else if (bmiCalculate >= 30 && bmiCalculate < 34.9) {
+        result.innerText = `Obesity (Class I): ${bmiCalculate}`;
+    } else if (bmiCalculate >= 35.5 && bmiCalculate < 39.9) {
+        result.innerText = `Obesity (Class II): ${bmiCalculate}`;
     } else {
-        result.innerText = `Extreme Obasity: ${bmi}`;
+        result.innerText = `Extreme Obasity: ${bmiCalculate}`;
     }
 }
